7d7698c437046e0a6fc37c833db20e1c35bb98c9
kartikvirendrar <kartikrajput357@gmail.com>
2026-01-25 17:28:05 +0530

    add minimax and cartesia logos

---
+++ src/shared/icons/CartesiaIcon.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CartesiaIcon = ({ className, ...props }) => {
+    return (
+        <img className={className} src='/cartesia-logo.svg' alt='Cartesia Icon' {...props} />
+    )
+}
+
+export default CartesiaIcon
