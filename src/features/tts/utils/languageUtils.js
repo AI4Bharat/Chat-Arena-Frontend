9344fac66437b2a61a6a00cf4e45c4dcecca7d2c
kartikvirendrar <kartikrajput357@gmail.com>
2026-01-27 14:01:44 +0530

    enable tamil acadmeic benchmarking

--- src/features/tts/utils/languageUtils.js
+++ src/features/tts/utils/languageUtils.js
@@ -70,7 +70,7 @@ export const getAvailableLanguages = (mode, models, modelAId, modelBId) => {
     }
 
     case 'academic': {
-      return ["hi"];
+      return ["hi", "ta"];
     }
 
     default:
