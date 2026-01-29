306bb9588d049ed9cab8e6306c8d1212bcd729d1
Ishvinder Sethi <ishvindersethi22@gmail.com>
2026-01-29 14:36:58 +0530

    Add 'bn' to academic language options

--- src/features/tts/utils/languageUtils.js
+++ src/features/tts/utils/languageUtils.js
@@ -70,7 +70,7 @@ export const getAvailableLanguages = (mode, models, modelAId, modelBId) => {
     }
 
     case 'academic': {
-      return ["hi", "ta"];
+      return ["hi", "ta", "bn"];
     }
 
     default:
