cb323e3c291029543777a1c4954b5b604e80e8b3
kartikvirendrar <kartikrajput357@gmail.com>
2026-02-24 16:13:19 +0530

    enable hi and ta for academic mode in TTS arena

--- src/features/tts/utils/languageUtils.js
+++ src/features/tts/utils/languageUtils.js
@@ -70,7 +70,7 @@ export const getAvailableLanguages = (mode, models, modelAId, modelBId) => {
     }
 
     case 'academic': {
-      return ["te", "bn", "kn", "ml", "mr", "or", "gu", "ur"];
+      return ["hi", "ta", "te", "bn", "kn", "ml", "mr", "or", "gu", "ur"];
     }
 
     default:
